b0a30e7a490b442aafb7feb212f44bbe34fbd286
shizukani-cp <to.maimai0606@gmail.com>
2024-08-03 12:47:27 +0900

    自己紹介の記事を追加

--- scripts/view_pages.js
+++ scripts/view_pages.js
@@ -1,5 +1,10 @@
 
-const contents = [];
+const contents = [
+    {
+        title:"自己紹介",
+        href:"20240803"
+    }
+];
 
 window.onload = () => {
     const pages_element = document.getElementById("pages");
@@ -8,7 +13,7 @@ window.onload = () => {
         const content_link_element = document.createElement("a");
         content_link_element.setAttribute("href", `contents/${content.href}/entry.html`);
         content_link_element.appendChild(document.createTextNode(content.title));
-        content_title_element.setAttribute("style", "font-size: 3rem")
+        content_title_element.setAttribute("style", "font-size: 3rem;");
         content_title_element.appendChild(content_link_element);
         pages_element.appendChild(content_title_element);
     });
