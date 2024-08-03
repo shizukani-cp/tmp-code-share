c6956329f88a728dd65bb715d39ea7d23366b12f
shizukani-cp <to.maimai0606@gmail.com>
2024-08-03 12:32:18 +0900

    一部を修正

--- scripts/view_pages.js
+++ scripts/view_pages.js
@@ -6,7 +6,7 @@ window.onload = () => {
     contents.forEach((content) => {
         const content_title_element = document.createElement("h3");
         const content_link_element = document.createElement("a");
-        content_link_element.setAttribute("href", `contents/${content.href}`);
+        content_link_element.setAttribute("href", `contents/${content.href}/entry.html`);
         content_link_element.appendChild(document.createTextNode(content.title));
         content_title_element.setAttribute("style", "font-size: 3rem")
         content_title_element.appendChild(content_link_element);
