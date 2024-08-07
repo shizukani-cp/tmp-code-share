29d6a25b95362964468ca04905cfab4e35a7bbfb
shizukani-cp <to.maimai0606@gmail.com>
2024-08-07 18:15:33 +0900

    日付も一覧に表示するよう変更

--- scripts/view_pages.js
+++ scripts/view_pages.js
@@ -6,15 +6,29 @@ const contents = [
     }
 ];
 
+function formatDate(date) {
+    const year = parseInt(date.substring(0, 4), 10);
+    const month = parseInt(date.substring(4, 6), 10) - 1;
+    const day = parseInt(date.substring(6, 8), 10);
+    const paddedMonth = month.toString().padStart(2, '0');
+    const paddedDay = day.toString().padStart(2, '0');
+    
+    return `    ${year}年${paddedMonth}月${paddedDay}日`;
+}
+
 window.onload = () => {
     const pages_element = document.getElementById("pages");
     contents.reverse().forEach((content) => {
         const content_title_element = document.createElement("h3");
+        const content_date_element = document.createElement("span");
         const content_link_element = document.createElement("a");
         content_link_element.setAttribute("href", `contents/${content.date}/entry.html`);
-        content_link_element.appendChild(document.createTextNode(content.title));
+        content_title_element.appendChild(document.createTextNode(content.title));
         content_title_element.setAttribute("style", "font-size: 3rem;");
-        content_title_element.appendChild(content_link_element);
-        pages_element.appendChild(content_title_element);
+        content_link_element.appendChild(content_title_element);
+        content_date_element.appendChild(document.createTextNode(formatDate(content.date)));
+        content_date_element.setAttribute("style", "font-size: 1rem; color: #e0e0e0;");
+        content_title_element.appendChild(content_date_element);
+        pages_element.appendChild(content_link_element);
     });
 };
