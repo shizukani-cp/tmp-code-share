f031d866ae6b962e21ba9c3e377993880f1c51c7
shizukani-cp <to.maimai0606@gmail.com>
2024-08-07 17:59:50 +0900

    順番を逆にして表示するよう変更

--- scripts/view_pages.js
+++ scripts/view_pages.js
@@ -8,7 +8,7 @@ const contents = [
 
 window.onload = () => {
     const pages_element = document.getElementById("pages");
-    contents.forEach((content) => {
+    contents.reverse().forEach((content) => {
         const content_title_element = document.createElement("h3");
         const content_link_element = document.createElement("a");
         content_link_element.setAttribute("href", `contents/${content.href}/entry.html`);
