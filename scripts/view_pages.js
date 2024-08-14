64998faacc73fbfb0420d579a90c27db31e36f11
shizukani-cp <to.maimai0606@gmail.com>
2024-08-14 13:33:21 +0900

    日付の表示にsmallタグを使うよう変更

--- scripts/view_pages.js
+++ scripts/view_pages.js
@@ -24,14 +24,14 @@ window.onload = () => {
     const pages_element = document.getElementById("pages");
     contents.reverse().forEach((content) => {
         const content_title_element = document.createElement("h3");
-        const content_date_element = document.createElement("span");
+        const content_date_element = document.createElement("small");
         const content_link_element = document.createElement("a");
         content_link_element.setAttribute("href", `contents/${content.date}/entry.html`);
         content_title_element.appendChild(document.createTextNode(content.title));
         content_title_element.setAttribute("style", "font-size: 3rem;");
         content_link_element.appendChild(content_title_element);
         content_date_element.appendChild(document.createTextNode(formatDate(content.date)));
-        content_date_element.setAttribute("style", "font-size: 1rem; color: #e0e0e0;");
+        content_date_element.setAttribute("style", "color: #e0e0e0;");
         content_title_element.appendChild(content_date_element);
         pages_element.appendChild(content_link_element);
     });
