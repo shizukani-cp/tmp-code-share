5bb9efd63775e4491872f143dd9a9c851ee63c92
shizukani-cp <to.maimai0606@gmail.com>
2024-08-15 12:45:33 +0900

    ミスを修正

--- scripts/create_index.js
+++ scripts/create_index.js
@@ -1,19 +1,20 @@
 const LEVEL_AND_SIZE = {
-    1:50,
-    2:40,
-    3:30,
-    4:25,
-    5:20,
-    6:15
+    1:25,
+    2:30,
+    3:15,
+    4:12,
+    5:10,
+    6:8
 }
 const SIDEBAR_ELEMENT = document.getElementById("sidebar");
 
 for (let element of document.querySelectorAll("h1,h2,h3,h4,h5,h6")) {
     const level = element.tagName[1];
-    element.setAttribute("name", element.innerText);
+    element.setAttribute("id", element.innerText);
     const index_element = document.createElement("a");
     index_element.appendChild(document.createTextNode(element.innerText));
     index_element.setAttribute("style", `font-size: ${LEVEL_AND_SIZE[level]}px; color: #ffffff; text-decoration:none;`);
     index_element.setAttribute("href", `#${element.innerText}`)
     SIDEBAR_ELEMENT.appendChild(index_element);
+    SIDEBAR_ELEMENT.appendChild(document.createElement("br"));
 }
