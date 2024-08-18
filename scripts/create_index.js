45937db4e489cc834be5f65fb6e10c8ec8642a0b
shizukani-cp <to.maimai0606@gmail.com>
2024-08-18 10:55:56 +0900

    インデックスが右側に来るよう修正

--- scripts/create_index.js
+++ scripts/create_index.js
@@ -11,10 +11,12 @@ const SIDEBAR_ELEMENT = document.getElementById("sidebar");
 for (let element of document.querySelectorAll("h1,h2,h3,h4,h5,h6")) {
     const level = element.tagName[1];
     element.setAttribute("id", element.innerText);
-    const index_element = document.createElement("a");
-    index_element.appendChild(document.createTextNode(element.innerText));
-    index_element.setAttribute("style", `font-size: ${LEVEL_AND_SIZE[level]}px; color: #ffffff; text-decoration:none;`);
-    index_element.setAttribute("href", `#${element.innerText}`)
+    const index_element = document.createElement("p");
+    index_element.setAttribute("style", `font-size: ${LEVEL_AND_SIZE[level]}px;`);
+    index_element.setAttribute("class", "index");
+    const link_element = document.createElement("a");
+    link_element.appendChild(document.createTextNode(element.innerText));
+    link_element.setAttribute("href", `#${element.innerText}`);
+    index_element.appendChild(link_element);
     SIDEBAR_ELEMENT.appendChild(index_element);
-    SIDEBAR_ELEMENT.appendChild(document.createElement("br"));
 }
